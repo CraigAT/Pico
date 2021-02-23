c37e7ee34940d700950c50a560049b9887f2b3a0
Craig Thomas <craigat@gmail.com>
2021-02-23 23:20:02 +0000

    Add Next_Turn and Separate Reaction Timing

--- Display/Pico F-Won/PicoFWon.py
+++ Display/Pico F-Won/PicoFWon.py
@@ -4,16 +4,18 @@
 import picodisplay as display
 import utime
 import math
+import random
 
 
 # Clear the screen
-def clear(background="black"):
+def clear_screen(background="black"):
     if background == "white":
         display.set_pen(255, 255, 255)
     else:
         display.set_pen(0, 0, 0)
     display.clear()
     display.update()
+
 
 # Setup display  for use
 def setup_screen():
@@ -22,15 +24,16 @@ def setup_screen():
     display.init(buf)
     display.set_backlight(1.0)
 
+
 # Draw the intro screen
 def intro():
-    clear("white")
-    
+    clear_screen("white")
+    # Display "Pico"
     pico_x_base = 30  # 93 works well for centre, 70 original
     pico_y_base = 20
     display.set_pen(0, 0, 0)
     display.text("Pico", pico_x_base, pico_y_base, 0, 3)
-    
+    # Display "F-Won"
     logo_x_base = 30  ## 70  original
     logo_y_base = 50
     display.set_pen(255, 0, 0)
@@ -39,7 +42,7 @@ def intro():
     display.text("o", logo_x_base + 60, logo_y_base, 0, 4)
     display.set_pen(255, 255, 255)
     display.rectangle(logo_x_base + 76,logo_y_base +4,4,4)
-    
+    # Display "By CT"
     name_x_base = 140  # 100 original
     name_y_base = 100
     display.set_pen(0, 0, 0)
@@ -47,57 +50,103 @@ def intro():
     display.update()
     utime.sleep(3)
     
-def start():
-    max_time = 3000
     
-    # Lights
-    clear()
-    display.set_pen(255, 0, 0)  # Red lights
-    for light in range(5):
-        display.circle(40+(light * 40), 50, 10)
-        display.update()
-        utime.sleep(1)
-    delay = math.fmod(utime.ticks_ms(), 4) + 1
-    utime.sleep(delay)  # Random delay for lights out
-    
-    # Go
-    display.set_pen(0, 255, 0)  # Green for GO
-    # Pick side randomly for the start button, indicated by GO
-    lane = math.fmod(utime.ticks_ms(), 2)
-    if lane == 0:
-        display.text("G0", 20, 105, 240, 2)
-    else:
-        display.text("G0", 200, 105, 240, 2)
-    # Remove lights
-    display.set_pen(0, 0, 0)
-    display.rectangle(20,35,200,30)
-    display.update()
-    
-    # Timing
+def time_user(side,max_time):
+    # Time user's reactions
     t1 = utime.ticks_ms()  # Start timer
     while True:
         utime.sleep_ms(1)
-        if ( (lane == 0) and (display.is_pressed(display.BUTTON_B)) and (display.is_pressed(display.BUTTON_Y) == False) ) \
-                or ( (lane == 1) and (display.is_pressed(display.BUTTON_Y)) and (display.is_pressed(display.BUTTON_B) == False) ):
+        if ( (side == "<") and (display.is_pressed(display.BUTTON_B)) and (display.is_pressed(display.BUTTON_Y) == False) ) \
+                or ( (side == ">") and (display.is_pressed(display.BUTTON_Y)) and (display.is_pressed(display.BUTTON_B) == False) ):
             t2 = utime.ticks_ms()  # End timer
             break
         elif utime.ticks_diff(utime.ticks_ms(),t1) > max_time:  # If 3 seconds has passed
             t2 = utime.ticks_add(t1,max_time)    # End timer, set to max time even if it's slightly passed 3 seconds
             break
-    # Results
-    td = utime.ticks_diff(t2,t1) / 1000
-    #time_text = "Reaction time: " + str(td)
-    time_text = "Reaction time: " + "{:0<5}".format(str(td))
-    #clear()
+    # Calculate reaction time and display results
+    reaction_time = utime.ticks_diff(t2,t1) / 1000
+    time_text = "T: " + "{:0<5}".format(str(reaction_time))  # Display reaction time to 3 decimals (ms)
+    clear_screen()
     display.set_pen(255, 255, 255)
-    display.text(time_text, 20, 10, 240, 2)
+    display.text(time_text, 50, 30, 240, 4)
     display.update()
-    utime.sleep(5)
+    utime.sleep(1)  # Pause to read reaction time
+    return reaction_time
+    
+    
+def start(lap_timer, max_time):
+    
+    # Draw the red start lights one at a time
+    clear_screen()
+    display.set_pen(255, 0, 0)
+    for light in range(5):
+        display.circle(40+(light * 40), 50, 10)
+        display.update()
+        utime.sleep(1)
+    delay = math.fmod(utime.ticks_ms(), 4) + 1
+    utime.sleep(delay)  # Random delay before go lights out
+    
+    # Remove lights
+    display.set_pen(0, 0, 0)
+    display.rectangle(20,35,200,30)
+    # No effect until next display.update()
+
+    # Click the correct side to "Go"
+    display.set_pen(0, 255, 0)  # Green for GO
+    # Pick side randomly for the start button, indicated by GO
+    #sides = ["<",">"]
+    #chosen_side = sides[int(math.fmod(utime.ticks_ms(), 2))]
+    side = random.choice(["<",">"])
+    if side == "<":
+        display.text("G0", 20, 105, 240, 2)
+    else:
+        display.text("G0", 200, 105, 240, 2)
+    # Cover up start lights and display "Go"
+    display.update()
+    
+    # Time the user's reaction
+    lap_timer += time_user(side,max_time)
+    return lap_timer
+    
+    
+def next_turn(lap_timer, max_time, direction, lead_time):
+    clear_screen()
+    utime.sleep_ms(500)
+    # Show straight-ahead until a turn
+    display.set_pen(255, 255, 0)
+    display.text("^", 118, 42, 240, 4)
+    display.text("/\\", 110, 50, 240, 4)
+    #display.text("", 110, 70, 240, 4)
+    display.update()
+    # Prepare the turn
+    display.set_pen(0, 0, 0)
+    display.clear()  # Chosen to not clear screen immediately
+    turn_text = direction * 3
+    # Display large chevrons indicating turn direction
+    display.set_pen(255, 255, 255)
+    display.text(turn_text, 100, 50, 240, 4)
+    # Label correct key with chevrons for the turn
+    display.set_pen(0, 255, 0)
+    if direction == "<":
+        display.text(turn_text, 20, 105, 240, 2)
+    else:
+        display.text(turn_text, 200, 105, 240, 2)
+    # Wait before displaying the chevrons, simulates waiting to arrive at the corner
+    utime.sleep(lead_time)
+    display.update()  # Show the turn direction now
+    # Time the user's reaction
+    lap_timer += time_user(direction,max_time)
+    return lap_timer
+    
 
 # Main Menu
 def menu():
-    clear()
+    clear_screen()
     display.set_pen(255, 255, 255)
+    display.text("Select Track:", 20, 20, 240, 2)
+    display.text("  Spa", 20, 50, 240, 2)
+    display.text("  Monaco", 20, 70, 240, 2)
+    display.set_pen(0, 255, 0)
     display.text("Start Game", 115, 105, 240, 2)
     display.update()
     while True:
@@ -108,11 +157,22 @@ def menu():
 
 ## Main Program
 while True:
+    lap_timer = 0
+    max_time = 2000
+    
     setup_screen()
-    intro()
+    #intro()
     menu()
-    start()
-    clear()
+    # Each race section adds time to the race_timer
+    lap_timer = start(lap_timer, max_time)
+    
+    ## Testing a single turn
+    lead_time = 5
+    direction = ">"
+    lap_timer = next_turn(lap_timer, max_time, direction, lead_time)
+    ##
+    
+    clear_screen()
     display.set_backlight(0)
     #utime.sleep(5)
     break
